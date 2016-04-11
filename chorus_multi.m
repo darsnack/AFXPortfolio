3f9a0982ea7415ed74fdd0ada024e4a55b52a7e0
daruwakj <daruwakj@rose-hulman.edu>
2016-04-11 15:57:10 -0400

    Added chorus_multi and portfolio directory

--- chorus_multi.m
+++ chorus_multi.m
@@ -1,4 +1,4 @@
-%% AFX -- Chorus effect
+%% AFX -- Chorus effect (multivoice)
 %
 % References:
 % http://www.mathworks.com/help/dsp/ref/dsp.variableintegerdelay-class.html
@@ -13,14 +13,17 @@ clear, close all
 %% User interface:
 
 % Effect parameters with suggested initial value and typical range:
-g_w = 0.5;
-LFO_freq_Hz = 0.05; % low-frequency oscillator rate (Hz) / 1Hz / 0.1 to 10Hz
+g_w1 = 1.7;
+g_w2 = 1.5;
+g_w3 = 1.7;
+g_d = 1;
+LFO_freq_Hz = 0.1; % low-frequency oscillator rate (Hz) / 1Hz / 0.1 to 10Hz
 LFO_depth_samples = 1000; % low-frequency oscillator depth (samples) / 5000 / 65536
-delay_max_ms = 20; % max delay line length (ms) / 0ms / 0 to 1000ms
+delay_max_ms = 30; % max delay line length (ms) / 0ms / 0 to 1000ms
                      % (the delay line max length is 65535 samples)
 
 % Source audio:
-file_name = 'Mixing Audio Textbook Samples\22 Other Modulation Tools\22-011 Chorus 1 (Guitar).wav';
+file_name = 'Mixing Audio Textbook Samples\22 Other Modulation Tools\22-001 Original Vocal.wav';
 audio_folder = 'D:\Users\Kyle\Documents\Courses\AFX\Samples';
 
 %% Create the audio reader and player objects
@@ -33,13 +36,29 @@ audio_player.QueueDuration = 0;
 delay_max = (delay_max_ms/1000)*audio_reader.SampleRate;
 
 %% Create the delay line object
-audio_delayline = dsp.VariableFractionalDelay;
-audio_delayline.MaximumDelay = delay_max;
+audio_delayline1 = dsp.VariableFractionalDelay;
+audio_delayline1.MaximumDelay = delay_max;
+
+audio_delayline2 = dsp.VariableFractionalDelay;
+audio_delayline2.MaximumDelay = delay_max;
+
+audio_delayline3 = dsp.VariableFractionalDelay;
+audio_delayline3.MaximumDelay = delay_max;
 
 %% Create the sinewave oscillators
-LFO = dsp.SineWave(0.5,LFO_freq_Hz);
-LFO.SampleRate = audio_reader.SampleRate;
-LFO.SamplesPerFrame = audio_reader.SamplesPerFrame;
+LFO1 = dsp.SineWave(0.5, LFO_freq_Hz);
+LFO1.SampleRate = audio_reader.SampleRate;
+LFO1.SamplesPerFrame = audio_reader.SamplesPerFrame;
+
+LFO2 = dsp.SineWave(0.5, LFO_freq_Hz);
+LFO2.PhaseOffset = pi/2;
+LFO2.SampleRate = audio_reader.SampleRate;
+LFO2.SamplesPerFrame = audio_reader.SamplesPerFrame;
+
+LFO3 = dsp.SineWave(0.5, LFO_freq_Hz);
+LFO3.PhaseOffset = 5*pi/4;
+LFO3.SampleRate = audio_reader.SampleRate;
+LFO3.SamplesPerFrame = audio_reader.SamplesPerFrame;
 
 %% Read, process, and play the audio
 while ~isDone(audio_reader)
@@ -50,14 +69,21 @@ while ~isDone(audio_reader)
     % x = step(test_tone);
     
     % Get the next low-frequency oscillator output frame
-    lfo = (step(LFO)+0.5)*LFO_depth_samples;
+    lfo1 = (step(LFO1) + 0.5) * LFO_depth_samples;
+    lfo2 = (step(LFO2) + 0.5) * LFO_depth_samples;
+    lfo3 = (step(LFO3) + 0.5) * LFO_depth_samples;
     
     % Retrieve the next frame from the delay line;
     % insert a new frame, too;
-    delayline_out = step(audio_delayline, x, lfo);
+    delayline_out1 = step(audio_delayline1, x, lfo1);
+    delayline_out2 = step(audio_delayline2, x, lfo2);
+    delayline_out3 = step(audio_delayline3, x, lfo3);
     
     % Generate the output
-    y = g_w .* delayline_out + x;
+    y = g_w1 .* delayline_out1 + ...
+        g_w2 .* delayline_out2 + ...
+        g_w3 .* delayline_out3 + ...
+        g_d .* x;
     
     % Listen to the results
     step(audio_player, y);
