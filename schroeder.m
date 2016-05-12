cda252c63ec1cc90907ff217180731d74500d498
daruwakj <daruwakj@rose-hulman.edu>
2016-05-12 14:51:43 -0400

    Fixed schroeder reverb

--- schroeder.m
+++ schroeder.m
@@ -10,7 +10,7 @@ clear, close all
 % an array of loop times can be specified
 % an appropriate number of comb and all pass filters will be used based on
 %  on the array length
-loop_times = [29.7 37.1 41.1 43.7]; % loop time (s) / 30 <= loop_times <= 45 
+loop_times = [29.7 37.1 41.1 43.7]; % loop time (ms) / 30 <= loop_times <= 45 
 RT60 = 5; % reverb time (s) / 5 / 1 <= RT60 <= 10
 
 % Results parameters
@@ -33,14 +33,17 @@ audio_player.QueueDuration = 0;
 
 %% Translate user interface parameters
 num_filters = length(loop_times);
-delay_samples = loop_times * audio_reader.SampleRate;
+delay_samples = loop_times / 1000 * audio_reader.SampleRate;
 g_comb = 10.^(-3*loop_times / RT60);
 
 delay_ap = zeros(round(num_filters/2));
+g_ap = zeros(round(num_filters/2));
 for i = 0:round(num_filters/2)-1
-    delay_ap(i + 1) = (100e-3 / (3^i)) * audio_reader.SampleRate;
+    tau = 100e-3 / (3^i);
+    reverb_time = 0.005 / (3^i);
+    delay_ap(i + 1) = tau * audio_reader.SampleRate;
+    g_ap(i + 1) = 10.^(-3 * tau / reverb_time);
 end
-g_ap = 0.708;
 
 %% Create the delay line object
 delay_lines_c = {};
@@ -73,12 +76,15 @@ while ~isDone(audio_reader)
     % Process all pass filters
     for i = 1:round(num_filters/2)
         tmp = reshape(y_ap(i, :, :), audio_reader.SamplesPerFrame, 2);
-        delayline_in = y + g_ap * tmp;
+        delayline_in = y + g_ap(i) * tmp;
         fun = @(a) step(a, delayline_in, delay_ap(i));
         tmp = cell2mat(cellfun(fun, delay_lines_ap(i), 'UniformOutput', false));
-        y = (1 - g_ap^2) * tmp - g_ap * x;
+        y = (1 - g_ap(i)^2) * tmp - g_ap(i) * x;
         y_ap(i, :, :) = reshape(tmp, 1, audio_reader.SamplesPerFrame, 2);
     end
+    
+    % Check for clipping
+    y = y / max(abs(y(:)));
     
     % Listen to the results
     step(audio_player, y);
